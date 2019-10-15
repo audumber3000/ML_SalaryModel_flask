644bbd7da1b8b82cc943be6b624b316ea8fbddd2
Kaustuv <kaustuv.kunal@gmail.com>
2019-10-15 20:20:06 +0530

    single page output

--- app.py
+++ app.py
@@ -7,18 +7,22 @@ app = Flask(__name__)
 
 
 @app.route("/")
+
 def root():
     return render_template("index.html")
 
 
 @app.route("/predict", methods=['POST'])
 def make_prediction():
+
     if request.method == 'POST':
         exp = request.form['exp']
         X = [[float(exp)]]
         [prediction] = loaded_model.predict(X)
+        salary = round(prediction, 2)
+    msg = "Standard salary for provided experience of  " + str(exp) + " years, would be: ₹ " + str(salary) + "/-- "
 
-    return render_template("predict.html", salary=round(prediction, 2), experience=exp)
+    return render_template("index.html", prediction_text= msg)
 
 
 if __name__ == '__main__':
